cdfa4db34e698f17d539f6c227ebd02823616209
flordeciruela <wendy.reyessan@gmail.com>
2017-03-07 02:09:04 -0500

    listo el dropdown

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,20 +23,20 @@ mostrarCoders = () => {
 home = () => { document.location.href = "index.html"; }
 
 //Drop
-	function myFunction() {
+function myFunction() {
 						document.getElementById("myDropdown").classList.toggle("show");
 						}
 
-	window.onclick = function(event) {
-					if (!event.target.matches('.dropbtn')) {
+window.onclick = function(event) {
 
-					var dropdowns = document.getElementsByClassName("dropdown-content");
-					var i;
-					for (i = 0; i < dropdowns.length; i++) {
-					var openDropdown = dropdowns[i];
-					if (openDropdown.classList.contains('show')) {
-							openDropdown.classList.remove('show');
-				}
-			}
-		}
-	}
+								 if (!event.target.matches('.dropbtn')) {
+										var dropdowns = document.getElementsByClassName("dropdown-content");
+										var i;
+
+								 for (i = 0; i < dropdowns.length; i++) {
+										 var openDropdown = dropdowns[i];
+										 if (openDropdown.classList.contains('show')) {
+												 openDropdown.classList.remove('show'); }
+										 }
+								 }
+							}
