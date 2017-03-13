c7083a9e94db614f0c84eb5ede3b746d40e76aab
flordeciruela <wendy.reyessan@gmail.com>
2017-03-13 02:07:54 -0500

    terminando funcionalidades

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,18 @@
 
-var user = prompt("Bienvenida! \n\n ¿Cuál es tu nombre?");
-
-var coders = [ { nombre : "Meche", apellido : "Zubieta",  promocion : "1era. promoción", estado : "Trabajando" },
-							 { nombre : "Arabela", apellido : "Rojas", promocion : "2da. promoción", estado : "Trabajando" },
-							 { nombre : "Maria", apellido : "Rosán",  promocion : "2da. promoción", estado : "Trabajando" },
-							 { nombre : "Guadalupe", apellido : "Racio",  promocion : "3era. promoción", estado : "Trabajando" },
-							 { nombre : "Sara", apellido : "Casa",  promocion : "3era. promoción", estado : "Trabajando" },
-							 { nombre : "Daniella", apellido : "Durán",  promocion : "4ta. promoción", estado : "Trabajando" },
-							 { nombre : "Michelle", apellido : "Seguil",  promocion : "5ta. promoción", estado : "Trabajando" },
-							 { nombre : "Diana", apellido : "Navarro",  promocion : "5ta. promoción", estado : "Trabajando" } ];
-
-mostrarCoders = () => {
-								for (var i in coders) {
-							 												document.getElementsByClassName("div-coder")[i].innerHTML +=
-																			"<p> Nombre: "+coders[i].nombre+"</p>"+
-										 							    "<p> Apellido: "+coders[i].apellido+"</p>"+
-										 							   	"<p> Promoción: "+coders[i].promocion+"</p>"+
-										 							    "<p> Estado: "+coders[i].estado+ "</p>";
-										 									}
-								};mostrarCoders();
-
-home = () => { document.location.href = "index.html"; }
+		//Funcion para guardar user:
+		var myParam = location.search.split('nombre=')[1]
+		var nombre = document.getElementById('usuario');
+		 if (myParam == undefined) { // Si esta vacio solicitar nombre
+				 var inicio = prompt('Cual es tu nombre');
+				 inicio = inicio.toString().toLowerCase();
+				 inicio = inicio.charAt(0).toUpperCase()+inicio.slice(1);
+				 nombre.innerHTML = inicio;
+		 } else { //Sino mostar nombre almacenado
+				 nombre.innerHTML = myParam;
+		 }
+		 document.getElementById("coders").addEventListener('click',function() {
+				 this.href += "?nombre="+nombre.innerHTML;
+		 });
 
 //Drop
 function myFunction() {
@@ -40,3 +32,69 @@ window.onclick = function(event) {
 										 }
 								 }
 							}
+
+			var mostrar= function(elementId) {
+	    document.getElementById("sprint-1").style.display="none";
+	    document.getElementById("sprint-2").style.display="none";
+	    document.getElementById("sprint-3").style.display="none";
+	    document.getElementById(elementId).style.display="block";
+	  }
+
+		//CORRECCION DE QUIZ SPRINT 1
+		var opcionesQuiz1= document.getElementsByClassName("quiz-1");
+		var contenedorQuiz1 = document.getElementById("contenedor-quiz1")
+		document.getElementById("enviar-sprint1").onclick=function(){
+			var contestadas = 0;
+
+			console.log(opcionesQuiz1)
+			for(var i =0;i<9;i++){
+				if(opcionesQuiz1[i].checked==true){
+					contestadas++
+				}
+			}
+			if(contestadas==3){
+				var correctas = 0;
+				if(opcionesQuiz1[0].checked){
+					correctas++
+				}
+				if(opcionesQuiz1[4].checked){
+					correctas++
+				}
+				if(opcionesQuiz1[8].checked){
+					correctas++
+				}
+				contenedorQuiz1.innerHTML = "<p class='resultado-quiz'>Tienes " + correctas + " correctas";
+			}
+			else{
+				alert("Todas las preguntas son obligatorias")
+			}
+		}
+
+			//Quiz sprint 2:
+			var opcionesQuiz2= document.getElementsByClassName("quiz-2");
+			var contenedorQuiz2 = document.getElementById("contenedor-quiz2")
+			document.getElementById("enviar-sprint2").onclick=function(){
+				var contestadas = 0;
+				for(var i =0;i<9;i++){
+					if(opcionesQuiz2[i].checked==true){
+						contestadas++
+					}
+				}
+				if(contestadas==3){
+					var correctas = 0;
+					if(opcionesQuiz2[1].checked){
+						correctas++
+					}
+					if(opcionesQuiz2[3].checked){
+						correctas++
+					}
+					if(opcionesQuiz2[7].checked){
+						correctas++
+					}
+					contenedorQuiz2.innerHTML = "<p class='resultado-quiz'>Tienes " + correctas + " correctas";
+				}
+				else{
+					alert("Todas las preguntas son obligatorias")
+				}
+
+			}
